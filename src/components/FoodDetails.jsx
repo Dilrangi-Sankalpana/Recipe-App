29b38e67b7252c837b9bf25d6c9994f07a8474a7
Dil0518 <dilrangi.s.karunathilaka@gmail.com>
2025-11-27 10:40:58 +0530

    Fetch food recipes

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function FoodDetails({ selectedFoodID }) {
+  const [foodRecipe, setFoodRecipe] = useState(null);
+
+  const URL = `https://api.spoonacular.com/recipes/${selectedFoodID}/information`;
+  const API_KEY = "dca2e049b42a42ea895400a3aa8ab213";
+
+  useEffect(() => {
+    const fetchFoodDetails = async () => {
+      if (!selectedFoodID) return;
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFoodRecipe(data);
+    };
+
+    fetchFoodDetails();
+  }, [selectedFoodID]);
+
   return <div>Food Details Component {selectedFoodID}</div>;
 }
