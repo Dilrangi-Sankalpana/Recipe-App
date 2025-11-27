98f1e76cb030d6ba9583084202fe1deec9bbed60
Dil0518 <dilrangi.s.karunathilaka@gmail.com>
2025-11-27 16:03:34 +0530

    Modify the food details component

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 
 export default function FoodDetails({ selectedFoodID }) {
-  const [foodRecipe, setFoodRecipe] = useState(null);
+  const [foodDetails, setFoodDetails] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const URL = `https://api.spoonacular.com/recipes/${selectedFoodID}/information`;
   const API_KEY = "dca2e049b42a42ea895400a3aa8ab213";
@@ -9,14 +10,51 @@ export default function FoodDetails({ selectedFoodID }) {
   useEffect(() => {
     const fetchFoodDetails = async () => {
       if (!selectedFoodID) return;
+
+      setLoading(true);
       const res = await fetch(`${URL}?apiKey=${API_KEY}`);
       const data = await res.json();
       console.log(data);
-      setFoodRecipe(data);
+      setFoodDetails(data);
+      setLoading(false);
     };
 
     fetchFoodDetails();
   }, [selectedFoodID]);
 
-  return <div>Food Details Component {selectedFoodID}</div>;
+  if (!selectedFoodID) return <p>Click a recipe to view details</p>;
+  if (loading) return <p>Loading recipe...</p>;
+  if (!foodDetails) return null;
+
+  return (
+    <div>
+      <div>
+        <h1>{foodDetails.title}</h1>
+        <img src={foodDetails.image} alt={foodDetails.title} />
+        <div>
+          <span>
+            🕛 <strong>{foodDetails.readyInMinutes} minutes</strong>
+          </span>
+          <span>
+            👨‍👩‍👧‍👦 <strong>Servings: {foodDetails.servings}</strong>
+          </span>
+          <span>
+            {foodDetails.vegetarian ? "🥕 Vegetarian" : "🍗 Non-Vegetarian"}
+          </span>
+          <span>{foodDetails.vegan ? "🥦 Vegan" : "🍳 Non-Vegan"}</span>
+        </div>
+        <div>
+          💲<span>{foodDetails.pricePerServing} per serving</span>
+        </div>
+      </div>
+      <div>
+        <h2>Instructions</h2>
+        <ul>
+          {foodDetails.analyzedInstructions[0].steps.map((step) => (
+            <li key={step.number}>{step.step}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
